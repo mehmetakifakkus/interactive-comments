00f9b15f489070b3d35cd06924f2255a46431a52
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-07 12:52:42 +0300

    Add Updating Comment as a utility

---
+++ app/components/comment/utils/updateComment.ts
@@ -0,0 +1,17 @@
+import { CommentProps } from "@/types";
+
+export const updateComment = (updatedComment: CommentProps, comments: CommentProps[]) => {
+  const updatedComments = comments.map((comment) => {
+    if (comment.id === updatedComment.id) {
+      return updatedComment;
+    }
+    comment.replies = comment.replies.map((reply) => {
+      if (reply.id === updatedComment.id) {
+        return updatedComment;
+      }
+      return reply;
+    });
+    return comment;
+  });
+  return updatedComments;
+}

718bbb0ce51db41da78bfb590c79f1be4978a243
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-07 12:52:16 +0300

    Add deleting functionality

--- app/components/DeleteComment.tsx
+++ app/components/DeleteComment.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Modal from "./Modal";
+import { useCommentContext } from "../context/CommentContext";
+import { CommentProps, ReplyComment } from "@/types";
 
 type Props = {
   isModalOpened: boolean;
   setIsModalOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  comment: CommentProps | ReplyComment;
 };
 
 export default function DeleteComment({
   isModalOpened,
   setIsModalOpened,
+  comment,
 }: Props) {
+  const { comments, setComments } = useCommentContext();
+
   return (
     <Modal isOpened={isModalOpened} setOpened={setIsModalOpened}>
       <h1 className="text-xl text-gray-600 font-semibold mb-4">
@@ -20,7 +26,19 @@ export default function DeleteComment({
         comment and can&apos;t be undone.
       </p>
       <div className="flex flex-row-reverse gap-2 mt-6 reverse">
-        <button className="text-sm bg-red-600 text-white px-6 py-2 rounded">
+        <button
+          onClick={() => {
+            const commentsCopy = [...comments];
+            // Remove the comment from the replies of the parent comment
+            commentsCopy.forEach((c) => {
+              c.replies = c.replies.filter((r) => r.id !== comment.id);
+            });
+            // Remove the comment from the comments list
+            setComments(commentsCopy.filter((c) => c.id !== comment.id));
+            setIsModalOpened(false);
+          }}
+          className="text-sm bg-red-600 text-white px-6 py-2 rounded"
+        >
           YES, DELETE
         </button>
         <button
